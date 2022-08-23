a3e01f006ee769f9e65ebf3e653813e53de3140e
LAPTOP-EHDI9A5M\sarat <sarath@arminotech.in>
2022-08-23 20:03:18 +0530

    footer added nd mobile view

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,12 @@ module.exports = defineConfig({
     'vuetify'
   ]
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/aws_community_calicut/'
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/aws_community_calicut/'
-    : '/'
+  publicPath: '/'
 }
